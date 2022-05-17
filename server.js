3aeeb56e08398cbfe674d01bb188d24c2be0cb82
Mugen <melton.sheridan@gmail.com>
2022-05-17 17:38:50 -0400

    didnt save everything before last commit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const db = require('./db/connection');
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -9,9 +8,11 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
